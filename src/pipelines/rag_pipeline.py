8b62ae2b7eeb581c1507ea119587c4ea388abae0
hungchiayu1 <72308196+hungchiayu1@users.noreply.github.com>
2024-04-01 05:53:27 +0800

    created rag pipeline that supports all 3 methods of retrieval in a single class

---
+++ src/pipelines/rag_pipeline.py
@@ -0,0 +1,176 @@
+
+import weaviate
+from DuRAG.generator import Generator
+from DuRAG.logger import logger
+from DuRAG.rds import db
+from DuRAG.reranker import Reranker
+from DuRAG.retriever.swr.swr_retriever import SentenceWindowRetriever
+import logging
+from DuRAG.retriever.amr.amr_retriever import AutoMergingRetriever
+from DuRAG.retriever.data_models import QueryObj, RagResponse, RetrievalObject
+BGE_QUERY_PREFIX = "Represent this sentence for searching relevant passages: "
+
+
+logging.getLogger("DuRAG").setLevel(logging.DEBUG)
+
+# Create a console handler and set the log level
+console_handler = logging.StreamHandler()
+console_handler.setLevel(logging.DEBUG)
+
+# Add the console handler to the logger
+logging.getLogger("DuRAG").addHandler(console_handler)
+
+def execute_sql(cursor, query, params):
+        try:
+            cursor.execute(query, params)
+            return cursor.fetchall()
+        except Exception as e:
+            logger.error(f"An error occurred executing SQL: {e}")
+            raise
+
+
+def get_pdf_names(cursor, filters):
+        return execute_sql(
+            cursor,
+            """SELECT pdf_document_name FROM "EXTRACTED_PDF" WHERE id IN %s""",
+            (tuple(filters),),
+        )
+    
+class RAGpipeline:
+
+
+    def __init__(self):
+        
+        self.reranker = Reranker()
+        self.client = weaviate.connect_to_local()
+        self.generator = Generator()
+        self.amr_retriever = AutoMergingRetriever(self.client,db)
+        self.swr_retriever = SentenceWindowRetriever(self.client)
+        self.client.close()
+        
+        
+        
+         
+
+    def pipeline(self,query_obj, alpha = 0.5,limit = 100,retrieve_type = 'swr'):
+        
+        self.client.connect()
+        with db.get_cursor() as rds_cursor:
+            names = [name for (name,) in get_pdf_names(rds_cursor, query_obj.filters)]
+            logger.info(names)
+    
+            pdf_name_to_id_map = dict(zip(names, query_obj.filters))
+            ## Doesn't matter which retriever we using as the hybrid search is inherited from base retriever
+            filter_params = self.amr_retriever._get_filter_param(
+                names, mode="or", property_name="pdf_name"
+            )
+            
+            logger.debug(f"Filter params: {filter_params}")
+            
+            bge_query = BGE_QUERY_PREFIX + query_obj.query
+            retrieval_response = self.amr_retriever.hybrid_search(
+                bge_query, 
+                filter_params=filter_params,
+                limit=limit,
+                alpha=alpha
+            )
+           
+            logger.debug(f"Retrieval response: {retrieval_response.objects}")
+            
+            
+            ## Do aggregation if retrieval type is amr
+            if retrieve_type == "amr":
+                
+                # aggregate chunks by 1 level
+                retrieval_objects = [
+                    RetrievalObject(
+                        uuid=str(chunk.uuid),
+                        query=query_obj.query,
+                        chunk=chunk.properties["content"],
+                        pdf_name=chunk.properties["pdf_name"],
+                        pdf_id = pdf_name_to_id_map[chunk.properties["pdf_name"]]
+                    )
+                    for chunk in retrieval_response.objects
+                ]
+                first_level_aggregation = self.amr_retriever.aggregate_chunks(retrieval_objects)
+                
+                # rerank after first level aggregation
+               
+                reranked_objects = self.reranker.rerank_top_k(
+                    first_level_aggregation,10
+                )
+                
+                # add more context to the chunks
+                reranked_objects = self.amr_retriever.aggregate_chunks(reranked_objects) 
+                
+
+            ## Do sentence window aggregation if retrieval type is swr
+            elif retrieve_type == "swr":
+                sentence_windows = self.swr_retriever.get_sentence_windows(
+                retrieval_response.objects)
+                    
+                logger.debug("Sentence Windows: ", sentence_windows)
+
+                retrieval_objects = [RetrievalObject(
+                    uuid=str(window_obj.center_uuid),
+                    query=query_obj.query,
+                    chunk=window_obj.joined_text(),
+                    pdf_name=window_obj.pdf_name,
+                    pdf_id = pdf_name_to_id_map[window_obj.pdf_name]
+                ) for window_obj in sentence_windows
+                ]
+                reranked_objects = self.reranker.rerank_top_k(retrieval_objects, 10)
+
+                # Prepare the context for the generator model by removing the query
+                
+            elif retrieve_type == 'fusion':
+                ## Get SWR objects
+                sentence_windows = self.swr_retriever.get_sentence_windows(
+                retrieval_response.objects)
+                    
+                logger.debug("Sentence Windows: ", sentence_windows)
+
+                swr_retrieval_objects = [ RetrievalObject(
+                    uuid=str(window_obj.center_uuid),
+                    query=query_obj.query,
+                    chunk=window_obj.joined_text(),
+                    pdf_name=window_obj.pdf_name,
+                    pdf_id = pdf_name_to_id_map[window_obj.pdf_name]
+                ) for window_obj in sentence_windows
+                ]
+              
+
+                ## GET AMR objects
+                amr_retrieval_objects = [
+                        RetrievalObject(
+                            uuid=str(chunk.uuid),
+                            query=query_obj.query,
+                            chunk=chunk.properties["content"],
+                            pdf_name=chunk.properties["pdf_name"],
+                            pdf_id = pdf_name_to_id_map[chunk.properties["pdf_name"]]
+                        )
+                        for chunk in retrieval_response.objects
+                    ]
+                first_level_aggregation = self.amr_retriever.aggregate_chunks(amr_retrieval_objects)
+                
+                
+                ## Get top 10 objects from swr and amr   
+            
+                reranked_objects = self.reranker.rerank_top_k(
+                    first_level_aggregation+swr_retrieval_objects,10
+                )
+                
+                
+                
+                
+            generator_context = [obj.chunk for obj in reranked_objects]
+            response = self.generator.response_synthesis(generator_context, query_obj.query)
+           
+        self.client.close()
+        
+        return RagResponse(message=response, chunks=reranked_objects)
+    
+if __name__ == "__main__":
+    test_query = QueryObj(query="What is ascent bridge", filters=[247, 305, 205])
+    rag = RAGpipeline()
+    rag.pipeline(test_query,alpha=0.5,limit=100,retrieve_type='swr')
--- src/retriever/amr/amr_retriever.py
+++ src/retriever/amr/amr_retriever.py
@@ -6,46 +6,50 @@ from DuRAG.retriever.retriever import Retriever
 
 
 class AutoMergingRetriever(Retriever):
-    def __init__(self, weaviate_client, rds_cursor):
+    def __init__(self, weaviate_client, db):
         super().__init__(weaviate_client, "AMR_chunks")
         self.weaviate_client = weaviate_client
-        self.rds_cursor = rds_cursor
+        self.db = db
+        #self.rds_cursor = rds_cursor
 
     def retrieve_by_uuid_from_weaviate(self, uuid):
+        
         return self.collection.query.fetch_object_by_id(uuid)
 
-    def retrieve_by_uuid_from_rds(self, uuid: str):
-        self.rds_cursor.execute(
-            f"""SELECT * FROM "amr_nodes" WHERE chunk_id = '{uuid}'"""
-        )
-        return self.rds_cursor.fetchall()
+    def retrieve_by_uuid_from_rds(self, uuid: str, rds_cursor):
+        
+        rds_cursor.execute(
+                f"""SELECT * FROM "amr_nodes" WHERE chunk_id = '{uuid}'"""
+            )
+        return rds_cursor.fetchall()
 
     def aggregate_chunks(self, retrieved_chunks: list[RetrievalObject]):
-        parents = defaultdict(list)
-        logger.info(f"AMR aggregate chunks received {len(retrieved_chunks)} chunks")
-        for chunk in retrieved_chunks:
-            # Retrieve the parent id for the given chunk
-            rds_row_info = self.retrieve_by_uuid_from_rds(str(chunk.uuid))
-            # If two or more chunks have the same parent, we keep the parent in the aggregation
-            parent_uuid = rds_row_info[0][1]
-            parents[parent_uuid].append(chunk)
-
-        # Now, replace the smaller chunks with their parent chunk
-        aggregated_chunks = []
-        for parent_uuid, chunks in parents.items():
-            # If the parent has more than one chunk, we only add the parent
-            if len(chunks) > 1:
-                logger.info(f"AMR aggregate chunks replacing {len(chunks)} with parent")
-                retrieved_parent = self.retrieve_by_uuid_from_rds(parent_uuid)
-                parent = RetrievalObject(
-                    uuid=parent_uuid,
-                    query=chunks[0].query,
-                    chunk=retrieved_parent[0][3],
-                    pdf_name=retrieved_parent[0][2],
-                )
-                aggregated_chunks.append(parent)
-            else:
-                # If the parent has only one chunk, we keep the original chunk
-                aggregated_chunks.extend(chunks)
+        with self.db.get_cursor() as rds_cursor:
+            parents = defaultdict(list)
+            logger.info(f"AMR aggregate chunks received {len(retrieved_chunks)} chunks")
+            for chunk in retrieved_chunks:
+                # Retrieve the parent id for the given chunk
+                rds_row_info = self.retrieve_by_uuid_from_rds(str(chunk.uuid),rds_cursor)
+                # If two or more chunks have the same parent, we keep the parent in the aggregation
+                parent_uuid = rds_row_info[0][1]
+                parents[parent_uuid].append(chunk)
+    
+            # Now, replace the smaller chunks with their parent chunk
+            aggregated_chunks = []
+            for parent_uuid, chunks in parents.items():
+                # If the parent has more than one chunk, we only add the parent
+                if len(chunks) > 1:
+                    logger.info(f"AMR aggregate chunks replacing {len(chunks)} with parent")
+                    retrieved_parent = self.retrieve_by_uuid_from_rds(parent_uuid,rds_cursor)
+                    parent = RetrievalObject(
+                        uuid=parent_uuid,
+                        query=chunks[0].query,
+                        chunk=retrieved_parent[0][3],
+                        pdf_name=retrieved_parent[0][2],
+                    )
+                    aggregated_chunks.append(parent)
+                else:
+                    # If the parent has only one chunk, we keep the original chunk
+                    aggregated_chunks.extend(chunks)
 
         return aggregated_chunks
